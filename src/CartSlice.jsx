0b2a6daafb9634d90b2431ef616c15fd182c51f2
Aromero62823 <anro9968@colorado.edu>
2026-04-01 08:27:02 -0600

    initialized reducer functions in the CartSlice.jsx file

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,14 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      console.log(`Just added: ${action.payload.name}!`)
-      state.items.push(action.payload);
+      const item = state.items.find(item => item.name === action.payload.name)
+      if(!item) {
+        state.items.push({...action.payload, quantity: 1})
+      } else {
+        item.quantity++;
+      }
     },
     removeItem: (state, action) => {
+      
+      if (state.item.some(item => item.name === action.payload)) {
+        state.items.filter(item => item.name !== action.payload)
+      }
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name, quantity} = action.payload
+      const item = state.items.find(item => item.name === name)
+      if (item) {
+        item.quantity = quantity;
+      }
     },
   },
 });
